8e3982651acfbce64df061b3a43f9a5d5b0d4cb2
FilippoGuarda <filippo.guarda96@gmail.com>
2025-11-22 20:47:13 +0100

    still working on updates to the setup for ubuntu machines, still needs some work, at the moment still some errors when transforming from odom to map for all robots except 6

--- workspace/src/aws-robomaker-hospital-world/launch/view_hospital.launch.py
+++ workspace/src/aws-robomaker-hospital-world/launch/view_hospital.launch.py
@@ -9,7 +9,7 @@ from launch.actions import SetEnvironmentVariable
 
 def generate_launch_description():
     ld = launch.LaunchDescription([
-        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value='$GAZEBO_MODEL_PATH:/home/colcon_ws/src/aws-robomaker-hospital-world/ignition_models'),
+        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value='$GAZEBO_MODEL_PATH:/home/workspace/src/aws-robomaker-hospital-world/ignition_models'),
         launch.actions.IncludeLaunchDescription(
             launch.launch_description_sources.PythonLaunchDescriptionSource(
                 [get_package_share_directory(
